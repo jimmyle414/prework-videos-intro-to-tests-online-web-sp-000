134b5da149cc283e7017f822681efd20881a67c4
Jimmy Le <jimmyle414@gmail.com>
2020-02-25 20:27:57 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -2,6 +2,7 @@ puts "Test are running..."
 
 describe "conversions" do
   describe "#ounces_to_grams" do
-    it "given 0, returns 0.0"
+    it "given 0, returns 0.0" do
+      grams = ounces_to_grams
   end
 end
