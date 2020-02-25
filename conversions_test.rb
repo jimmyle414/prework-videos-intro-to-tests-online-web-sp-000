1acb002afb6e0653e2b05a4e7d34f6d147e88aa9
Jimmy Le <jimmyle414@gmail.com>
2020-02-25 20:20:33 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -7,3 +7,11 @@ if answer == 0.0
 else
   puts "Test failed: got #{answer} instead"
 end
+
+puts "ounces to grams, when given 1, returns 28.3495"
+answer = ounces_to_grams(1)
+if answer == 28.3495
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
+end
